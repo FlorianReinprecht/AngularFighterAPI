3b22fb5e143ed698ed1a1ef5897d1d05aacfb32a
Florian Reinprecht <florian.reinprecht@edu.fh-joanneum.at>
2018-01-11 18:46:42 +0100

    add Params to search methods of Engines

--- src/main/java/at/fh/ima/swengs/angularfighter/api/model/FireEngineAbbreviation.java
+++ src/main/java/at/fh/ima/swengs/angularfighter/api/model/FireEngineAbbreviation.java
@@ -5,7 +5,7 @@ import javax.persistence.*;
 
 
 @Entity
-@Table(name="FireEngineAbbreaviation")
+@Table(name="FireEngineAbbreviation")
 public class FireEngineAbbreviation {
 	
 	
--- src/main/java/at/fh/ima/swengs/angularfighter/api/model/FireEngineRepository.java
+++ src/main/java/at/fh/ima/swengs/angularfighter/api/model/FireEngineRepository.java
@@ -1,6 +1,7 @@
 package at.fh.ima.swengs.angularfighter.api.model;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,18 +12,18 @@ public interface FireEngineRepository extends PagingAndSortingRepository<FireEng
 	
 	@Transactional
 	//FireEngine findByBuildYear(String buildYear);
-	public List<FireEngine> findByFireBrigade(FireBrigade fireBrigade);
-	public List<FireEngine> findByFireBrigade_Id(int id);
+	//public List<FireEngine> findByFireBrigade(FireBrigade fireBrigade);
+	public List<FireEngine> findByFireBrigade_Id(@Param("id")  long id);
 	public List<FireEngine> findAllByOrderByModel();
 	public List<FireEngine> findAllByOrderByPerformance();
 	public List<FireEngine> findAllByOrderByBuildYear();
-	public List<FireEngine> findByActive(String active);
-	public List<FireEngine> findByAbbreviation_Abbreviation(String abbreviation);
-	public List<FireEngine> findByModelContainingAllIgnoreCase(String searchString);
+	public List<FireEngine> findByActive(@Param("active") String active);
+	public List<FireEngine> findByAbbreviation_Abbreviation(@Param("abbreviation") String abbreviation);
+	public List<FireEngine> findByModelContainingAllIgnoreCase(@Param("searchString") String searchString);
 	
-	public FireEngine findById(int id);
-	public FireEngine findByFireBrigadeAndId(FireBrigade fireBrigade, int id);
+	public FireEngine findById(@Param("id")long id);
+	//public FireEngine findByFireBrigadeAndId(FireBrigade fireBrigade, long id);
 
-	public void deleteByFireBrigadeAndId(FireBrigade fireBrigade, int id);
+	//public void deleteByFireBrigadeAndId(FireBrigade fireBrigade, long id);
 	
 }
