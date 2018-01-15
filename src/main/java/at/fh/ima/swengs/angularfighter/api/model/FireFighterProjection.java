c325b8ba6e432cd3a7c1ec7106f6057c24726533
Dominik Krueger <dominik.krueger@edu.fh-joanneum.at>
2018-01-15 19:36:24 +0100

    add projection for firefighters

---
+++ src/main/java/at/fh/ima/swengs/angularfighter/api/model/FireFighterProjection.java
@@ -0,0 +1,13 @@
+package at.fh.ima.swengs.angularfighter.api.model;
+
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name = "all" , types = FireFighter.class)
+public interface FireFighterProjection {
+    Long getId();
+    String getTitle();
+    String getSurname();
+    String getName();
+    FireBrigade getFireBrigade();
+
+}
