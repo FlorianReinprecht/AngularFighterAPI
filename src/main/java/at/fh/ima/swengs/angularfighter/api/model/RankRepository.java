7b9dabf2a885a4f077d309ca48fbab3f7b1ff4d3
Dominik Krueger <dominik.krueger@edu.fh-joanneum.at>
2018-01-22 13:44:22 +0100

    update rankRepository

--- src/main/java/at/fh/ima/swengs/angularfighter/api/model/RankRepository.java
+++ src/main/java/at/fh/ima/swengs/angularfighter/api/model/RankRepository.java
@@ -1,16 +1,20 @@
 package at.fh.ima.swengs.angularfighter.api.model;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RepositoryRestResource
 
 public interface RankRepository extends PagingAndSortingRepository<Rank, Long> {
 
 	@Transactional
-    Rank findByAbbreviation(String abbreviation);
+    //Rank findByAbbreviation(String abbreviation);
 
 	public Rank findById(long id);
-
+	public List<Rank> findByAbbreviationContaining(@Param("abbreviation")  String abbreviation);
+	public List<Rank> findByDescriptionContaining(@Param("description")  String description);
 }
