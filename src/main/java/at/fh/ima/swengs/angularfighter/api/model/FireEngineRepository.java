f3fe94d0125399fb2f86f4264f673a7cc54b3af1
Dominik Krueger <dominik.krueger@edu.fh-joanneum.at>
2018-01-22 15:23:28 +0100

    update fireEngineRepository

--- src/main/java/at/fh/ima/swengs/angularfighter/api/model/FireEngineRepository.java
+++ src/main/java/at/fh/ima/swengs/angularfighter/api/model/FireEngineRepository.java
@@ -14,11 +14,16 @@ public interface FireEngineRepository extends PagingAndSortingRepository<FireEng
 	//FireEngine findByBuildYear(String buildYear);
 	//public List<FireEngine> findByFireBrigade(FireBrigade fireBrigade);
 	public List<FireEngine> findByFireBrigade_Id(@Param("id")  long id);
+	public List<FireEngine> findByModelContaining(@Param("model")  String model);
+	public List<FireEngine> findByLicensePlateContaining(@Param("plate")  String plate);
+	public List<FireEngine> findByAbbreviation_AbbreviationContaining(@Param("abbreviation")  String abbreviation);
+	public List<FireEngine> findByActive(@Param("active") Boolean active);
+
 	public List<FireEngine> findAllByOrderByModel();
 	public List<FireEngine> findAllByOrderByPerformance();
 	public List<FireEngine> findAllByOrderByBuildYear();
-	public List<FireEngine> findByActive(@Param("active") String active);
-	public List<FireEngine> findByAbbreviation_Abbreviation(@Param("abbreviation") String abbreviation);
+
+	//public List<FireEngine> findByAbbreviation_Abbreviation(@Param("abbreviation") String abbreviation);
 	public List<FireEngine> findByModelContainingAllIgnoreCase(@Param("searchString") String searchString);
 	
 	public FireEngine findById(@Param("id")long id);
