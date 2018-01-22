8c0c276f863e246f9a8606eefae472f21d717d87
Dominik Krueger <dominik.krueger@edu.fh-joanneum.at>
2018-01-22 16:22:58 +0100

    update fireEngineAbbreviationRepository

--- src/main/java/at/fh/ima/swengs/angularfighter/api/model/FireEngineAbbreviationRepository.java
+++ src/main/java/at/fh/ima/swengs/angularfighter/api/model/FireEngineAbbreviationRepository.java
@@ -1,15 +1,19 @@
 package at.fh.ima.swengs.angularfighter.api.model;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface FireEngineAbbreviationRepository extends PagingAndSortingRepository<FireEngineAbbreviation, Long> {
 
 	@Transactional
-	FireEngineAbbreviation findByAbbreviation(String abbreviation);
+	//FireEngineAbbreviation findByAbbreviation(String abbreviation);
 
 	public FireEngineAbbreviation findById(int id);
-	
+	public List<FireEngineAbbreviation> findByAbbreviationContaining(@Param("abbreviation")  String abbreviation);
+	public List<FireEngineAbbreviation> findByDescriptionContaining(@Param("description")  String description);
 }
