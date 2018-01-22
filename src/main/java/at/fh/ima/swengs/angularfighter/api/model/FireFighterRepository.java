31f11a421d92ca478a53b3140b95c59947191473
Dominik Krueger <dominik.krueger@edu.fh-joanneum.at>
2018-01-22 13:14:23 +0100

    update fireFighterRepository

--- src/main/java/at/fh/ima/swengs/angularfighter/api/model/FireFighterRepository.java
+++ src/main/java/at/fh/ima/swengs/angularfighter/api/model/FireFighterRepository.java
@@ -13,10 +13,13 @@ import org.springframework.transaction.annotation.Transactional;
 public interface FireFighterRepository extends PagingAndSortingRepository<FireFighter, Long> {
 
 	@Transactional
-	public List<FireFighter>findBySurname(String surName);
 	public List<FireFighter>findByFireBrigade(FireBrigade fireBrigade);
 	public List<FireFighter>findByFireBrigade_Id(@Param("id")  long id);
-	public List<FireFighter>findByName(String name);
+
+	public List<FireFighter>findByName(@Param("name")  String name);
+	public List<FireFighter>findBySurname(@Param("surname")  String surname);
+	public List<FireFighter>findByFireFighterStatus_id(@Param("status")  long statusId);
+
 	public List<FireFighter>findByRank_Abbreviation(String rank);
 	public List<FireFighter>findByFireFighterStatus_Description(String status);
 	public List<FireFighter>findByGender(String gender);
